62347f93ebc5909dd10e85a2525f87303e73d66b
Fredy Lopez <fredylopez@Fredys-iMac.local>
2017-02-27 14:12:21 -0800

    updated webview to open inside the app

--- app/src/main/java/com/advanstaff/essapp/MainActivity.java
+++ app/src/main/java/com/advanstaff/essapp/MainActivity.java
@@ -1,19 +1,71 @@
 package com.advanstaff.essapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.MainActivity);
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("http://avfee.prismhr.com/avf.demo/cmd/login");
+
+        mWebView = (WebView) findViewById(R.id.MainActivity);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(false);
+
+        // Use remote resource
+        mWebView.loadUrl("https://avfee.prismhr.com/avf.demo/cmd/login");
+
+        // Stop local links and redirects from opening in browser instead of WebView
+        // mWebView.setWebViewClient(new MyAppWebViewClient());
+
+        // Use local resource
+        // mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
